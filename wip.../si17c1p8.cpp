1c0c9a9390c29ec3acaf8d74cd108dcea783836a
sarinali <74844953+sarinali@users.noreply.github.com>
2021-03-03 20:01:00 -0500

    doesnt WORK :(

--- wip.../si17c1p8.cpp
+++ wip.../si17c1p8.cpp
@@ -9,13 +9,18 @@ int main(){
     for (int i=0;i<n;i++) {
         cin >> val[i];
     }
-    for (int i = 0; i <= v; i++) {
-        dp[i][0] = 1;
-    }
+    // for (int i = 0; i <= v; i++) {
+    //     dp[0][i] = 1;
+    // }
+    dp[0][0] = 1;
     for (int i=1; i <= n; i++) {
         for (int j=1; j <= v; j++) {
-            if (j >= val[i-1])
-                dp[i][j] = dp[i-1][j] + dp[i][j-val[i-1]];
+            if (val[i-1] <= j) {
+                dp[i][j] = dp[i-1][j] + dp[i-1][j-val[i-1]];
+            }
+            // else {
+            //     dp[i][j] = dp[i-1][j];
+            // }
         }
     }
     cout << dp[n][v];
