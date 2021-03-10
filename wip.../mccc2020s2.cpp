a4d8b9893742189a5f24292e0a125f3617d2aee8
sarinali <74844953+sarinali@users.noreply.github.com>
2021-03-10 09:41:56 -0500

    :(

--- wip.../mccc2020s2.cpp
+++ wip.../mccc2020s2.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
-
+int grid[2001][2001];
+bool c[2001];
+bool r[2001];
+int n;
 int main(){
+    cin >> n;
+    for (int i = 0;i < n; i++) {
+        for (int j = 0;j < n; j++) {
+            cin >> grid[i][j];
+            if (grid[i][j] == 1) {
+                c[i] = true;
+                r[j] = true;
+            }
+        }
+    }
     return 0;
 }
