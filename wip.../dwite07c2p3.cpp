c24a00e104e86b79710d2c0b302baac1041784ba
sarinali <74844953+sarinali@users.noreply.github.com>
2021-03-12 18:01:27 -0500

    new

---
+++ wip.../dwite07c2p3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int r, c;
+char rm[41][41];
+int a[6];
+int vis[41][41];
+void bfs() {
+    list <int> queue;
+
+}
+int main() {
+    cin >> r >> c;
+    for (int i = 0; i < r; i++) {
+        for (int j =0; i< c; j++) {
+            cin >> rm[i][j];
+        }
+    }
+}
