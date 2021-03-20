fe5a0f52fd896b8c40de3fca7d43399858e64c9c
sarinali <74844953+sarinali@users.noreply.github.com>
2021-03-19 23:57:10 -0400

    incomplete, almost done?

--- wip.../ccc16s3.cpp
+++ wip.../ccc16s3.cpp
@@ -1,25 +1,54 @@
 #include <iostream>
 #include <set>
+#include <stack>
 using namespace std;
+typedef pair<int, int> pi; 
 int n,m;
 bool ar[100000];
+bool vis[100000];
+int dis[100000];
 set<int> adj[100000];
+pi dfs(int node) {
+    pi max = make_pair(node, 0);
+    stack<int> s;
+    s.push(node);
+    while (!s.empty()) {
+        int cur = s.top();
+        s.pop();
+        set<int>::iterator it;
+        for (it = adj[cur].begin(); it != adj[cur].end(); it++){
+            if (!vis[*it]) {
+                s.push(*it);
+                vis[*it] = true;
+                dis[*it] = dis[cur] +1;
+                if (dis[*it] > max.second) {
+                    max.first = *it;
+                    max.second = dis[*it];
+                }
+            }
+        }
+    }
+    return max;
+}
 int main() {
     cin >> n >> m;
     for (int i =0; i< m; i++) {
         int x;
+        cin >> x;
         ar[x] = true;
     }
     for (int i =0; i < n-1; i++) {
         int x, y;
+        cin >> x >> y;
         adj[x].insert(y);
         adj[y].insert(x);
     }
+    // cout << ar[5];
     int count;
     while (true) {
         count = n;
         for (int i= 0; i < n; i++) {
-            if (adj[i].size() == 1) {
+            if (adj[i].size() == 1 && ar[i] == false) {
                 set<int>::iterator it;
                 int first;
                 for (it = adj[i].begin(); it != adj[i].end(); it++){
@@ -28,6 +57,7 @@ int main() {
                 if (ar[first] == false) {
                     adj[first].erase(i);
                     adj[i].erase(first);
+                    // cout << i << " " << first << endl;
                     count--;
                 }
             }
@@ -36,12 +66,23 @@ int main() {
             break;
         }
     }
-    for (int i = 0;i < n; i++) {
-        cout << endl;
-        set<int>::iterator it;
-        for (it = adj[i].begin(); it != adj[i].end(); it++) {
-            cout << *it << " ";
-        }
-    }
+    vis[0] = true;
+    int newnode = dfs(0).first;
+    for (int i = 0; i < n; i++){
+        vis[i] = false;
+        dis[i] = false;
+    }    
+    vis[newnode] = true;
+    int diameter = dfs(newnode).second;
+    cout << diameter << endl;
+    // for (int i = 0;i < n; i++) {
+    //     cout << i << " ";
+    //     set<int>::iterator it;
+    //     for (it = adj[i].begin(); it != adj[i].end(); it++) {
+    //         cout << *it << " ";
+    //     }
+    //     cout << endl;
+    // }
+
     return 0;
 }
