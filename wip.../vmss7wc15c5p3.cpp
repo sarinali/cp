ff5d0b9d5334205103347891e74bdf9d53288d81
sarinali <74844953+sarinali@users.noreply.github.com>
2021-04-15 16:53:43 -0400

    idk wa (at least not rte?)

--- wip.../vmss7wc15c5p3.cpp
+++ wip.../vmss7wc15c5p3.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
 typedef long long ll;
-const int M = 1e3;
+const int M = 2*1e3+5;
 int n;
 const int MOD = 1e9+7;
 ll sum;
-int BIT[M+5][M+5];
-ll findsum(int r, int c, int x) {
+int BIT[M][M];
+ll findsum(int r, int c) {
     ll cursum = 0;
-    int ind = x;
-    while (ind >0) {
+    int ind = r;
+    while (ind>0) {
         cursum += BIT[r+c][ind];
         ind -= ind&-ind;
     }
     return cursum;
 }
 void update(int r, int c, int t) {
-    // BIT[r+c][c] += t;
-    int ind = c;
-    while (ind <= n) {
+    int ind = r;
+    while (ind <= M) {
         BIT[r+c][ind]+=t;
         ind+=ind&-ind;
     }
@@ -31,12 +30,12 @@ int main() {
         cin >> op;
         if (op == 1) {
             cin >> r >> c >> t;
-            // update(r, c, t);
+            update(r, c, t);
         } else if (op == 2) {
             cin >> r >> c >> t;
-            // sum += findsum(r, c, t);
+            sum += findsum(r, c)-findsum(r, c-t-1);
         }
     }
-    printf("%lld", sum%MOD);
+    printf("%lld\n", sum%MOD);
     return 0;
 }
