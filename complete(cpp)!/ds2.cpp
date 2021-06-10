6f5bcbc60b5278dac9f8a8b1d8e26351108b3e9e
sarinali <74844953+sarinali@users.noreply.github.com>
2021-06-10 19:23:53 -0400

    m

--- complete(cpp)!/ds2.cpp
+++ complete(cpp)!/ds2.cpp
@@ -4,7 +4,7 @@ typedef pair<int, int> pi;
 const int M = 1e6;
 int n, m, ht[M+1], parent[M+1];
 bool c;
-vector <pi> v;
+vector <int> v;
 int find(int node) {
     if (parent[node] != node) {
         parent[node] = find(parent[node]);
@@ -26,31 +26,32 @@ void unite(int a, int b) {
     }
 }
 int main() {
-    cin >> n >> m;
     cin.tie(0); cin.sync_with_stdio(0);
+    cin >> n >> m;
     for (int i = 1; i <= n; i++) {
         parent[i] = i;
     }
     for (int i = 0; i < m; i++) {
         int a, b;
+        cin >> a >> b;
         if (find(a) != find(b)) {
             unite(a,b);
-            pi p = {a, b};
-            v.push_back(p);
+            v.push_back(i+1);
         }
     }
     int par = parent[1];
     for (int i = 1; i <= n; i++) {
-        if (parent[i] != par) {
+        int u = find(i);
+        if (u != par) {
             c = true;
             break;
-        } 
+        }
     }
     if (c) {
         cout << "Disconnected Graph" << endl;
     } else {
         for (int i = 0; i < v.size(); i++) {
-            cout << v.at(i).first << " " <<  v.at(i).second << endl;
+            cout << v.at(i) << endl;
         }
     }
     return 0;
